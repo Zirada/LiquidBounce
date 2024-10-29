d1c87bf13d59b9bbbdeb1b2e76b2499a71be18a1
Zirada <185837150+Zirada@users.noreply.github.com>
2024-10-29 19:24:40 +0000

    Create ModuleFreeLook.kt

---
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/ModuleFreeLook.kt
@@ -0,0 +1,64 @@
+/*
+ * This file is part of LiquidBounce (https://github.com/CCBlueX/LiquidBounce)
+ *
+ * Copyright (c) 2015 - 2024 CCBlueX
+ *
+ * LiquidBounce is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LiquidBounce is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.ccbluex.liquidbounce.features.module.modules.render
+
+import net.ccbluex.liquidbounce.features.module.Category
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.utils.input.InputBind
+import net.minecraft.client.option.Perspective
+import net.minecraft.client.option.Perspective.*
+
+object ModuleFreeLook : Module("FreeLook", Category.RENDER, disableOnQuit = true, bindAction = InputBind.BindAction.HOLD) {
+
+    private val noPitchLimit by boolean("NoPitchLimit", true)
+
+    private var previousPerspective: Perspective? = null
+    var cameraYaw = 0f
+    var cameraPitch = 0f
+
+    override fun enable() {
+        cameraYaw = player.yaw
+        cameraPitch = player.pitch
+
+        previousPerspective = mc.options.perspective
+        mc.options.perspective = THIRD_PERSON_BACK
+    }
+
+    override fun disable() {
+        cameraYaw = player.yaw
+        cameraPitch = player.pitch
+
+        mc.options.perspective = previousPerspective
+    }
+
+    fun onPerspectiveChange() {
+        enabled = false
+        mc.options.perspective = previousPerspective?.next() ?: FIRST_PERSON
+    }
+
+    fun updateCamera(cursorDeltaX: Float, cursorDeltaY: Float) {
+        cameraYaw += cursorDeltaX * 0.15f
+        cameraPitch += cursorDeltaY * 0.15f
+
+        if (!noPitchLimit) {
+            cameraPitch = cameraPitch.coerceIn(-90f..90f)
+        }
+    }
+}
